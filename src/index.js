df4d8836abd0683d54617a71b68a6b53e96c0570
Anna Clara <anisferraz@gmail.com>
2024-01-01 17:27:04 -0300

    feat: add chakra provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ShopContextProvider } from './contexts/shopContext';
+import { ChakraProvider } from "@chakra-ui/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ShopContextProvider>
-      <App />
-    </ShopContextProvider>
+    <ChakraProvider>
+      <ShopContextProvider>
+        <App />
+      </ShopContextProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
