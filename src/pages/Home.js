dfd80786988c6bc434153770e07b8e7c159057df
Anna Clara <anisferraz@gmail.com>
2024-01-01 17:28:48 -0300

    feat: add elements on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Grid, Text, Image } from "@chakra-ui/react"
 import { ShopContext } from '../contexts/shopContext'
 
 export function Home() {
@@ -12,14 +14,20 @@ export function Home() {
   if (!products) return <div>loading...</div>
   
   return (
-    <div>
-      {
-        products.map(product => (
-          <div key={product.id}>
-            <h3>{product.title}</h3>
-          </div>
-        ))
-      }
-    </div>
+    <Box>
+      <Grid templateColumns={['repeat(1fr)', 'repeat(3, 1fr)']}>
+        {
+          products.map(product => (
+            <Link to={`/products/${product.handle}`} key={product.id}>
+              <Box _hover={{ opacity: '80%' }} textAlign="center" position="relative">
+                <Image src={product.images[0].src} />
+                <Text>{product.title}</Text>
+                <Text>${product.variants[0].price.amount}</Text>
+              </Box>
+            </Link>
+          ))
+        }
+      </Grid>
+    </Box>
   );
 }
