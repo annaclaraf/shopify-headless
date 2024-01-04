ee165e53662bbe5b098e6774f4b79c4b2b735c3b
Anna Clara <anisferraz@gmail.com>
2024-01-04 20:35:34 -0300

    feat: CartDrawer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Product } from './pages/Product'
 import { NavBar } from './components/NavBar'
+import { CartDrawer } from './components/CartDrawer'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
+        <CartDrawer />
         <Routes>
           <Route path="/products/:handle" element={<Product />} />
           <Route path="/" element={<Home />} />
