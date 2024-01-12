d45344778a439efff148e7fc5fa8c3d7c9c0dd38
Anna Clara <anisferraz@gmail.com>
2024-01-11 21:01:34 -0300

    feat: image with text component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Box, Grid, Text, Image } from "@chakra-ui/react"
 import { ShopContext } from '../contexts/shopContext'
 import { Hero } from '../components/Hero';
+import { ImageWithText } from '../components/ImageWithText';
 
 export function Home() {
   const { handleFetchAllProducts, products } = useContext(ShopContext);
@@ -30,6 +31,19 @@ export function Home() {
           ))
         }
       </Grid>
+      <ImageWithText
+        button
+        image="https://cdn.shopify.com/s/files/1/2248/5209/files/banner-dog-2.jpg"
+        heading="Lorem Ipsum" 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac nisi pulvinar, malesuada risus vel, pellentesque nibh. Nunc aliquet dolor id arcu porta, vitae tristique lacus vestibulum. Aliquam dapibus lectus ac eros rhoncus, vel accumsan arcu pulvinar. Sed pharetra, magna sed pulvinar ullamcorper, elit neque finibus metus, nec vehicula leo risus sed mi. Nulla vitae lobortis turpis." 
+      />
+      <ImageWithText 
+        reverse
+        button
+        image="https://cdn.shopify.com/s/files/1/2248/5209/files/banner-dog-1.jpg?v=1705016318"
+        heading="Lorem Ipsum" 
+        text="Morbi volutpat tellus non libero volutpat, sed auctor ligula rhoncus. Vestibulum odio diam, fringilla ac sem imperdiet, lobortis vulputate leo. Phasellus ac nunc ullamcorper, pellentesque mauris vitae, posuere nulla. Quisque ante orci, iaculis in eleifend posuere, fringilla ac lectus. Fusce efficitur arcu at augue placerat consequat." 
+      />
     </Box>
   );
 }
